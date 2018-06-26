868ed2771b1b4699f75780cb311d06e3e1f53664
MehmetSalihK <37173211+MehmetSalihK@users.noreply.github.com>
2018-06-26 15:27:57 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -23,7 +23,7 @@ function generateMessages(){
 
 
 bot.on("message", message => {
-    if (message.author.id == message.content.toLowerCase() == setupCMD){
+    if (message.author.id = message.content.toLowerCase() == setupCMD){
         var toSend = generateMessages();
         let mappedArray = [[toSend[0], false], ...toSend.slice(1).map( (message, idx) => [message, reactions[idx]])];
         for (let mapObj of mappedArray){
